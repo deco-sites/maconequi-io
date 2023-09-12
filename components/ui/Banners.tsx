f192bdde0b5b66fc6408e5b791104b2e07e95fc1
hugobp51 <hb.porto@hotmail.com>
2023-09-12 08:04:48 -0300

    Feat:Made component Banners responsive

--- components/ui/Banners.tsx
+++ components/ui/Banners.tsx
@@ -1,5 +1,8 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
+import Slider from "$store/components/ui/Slider.tsx";
+import SliderJS from "$store/islands/SliderJS.tsx";
+import { useId } from "$store/sdk/useId.ts";
 
 export interface BannerProps {
   image: ImageWidget;
@@ -22,22 +25,42 @@ export function Banner({ image, title, subTitle }: BannerProps) {
         height={292}
         alt={""}
         src={image}
-        class="max-w-full max-h-full"
+        class="max-w-full max-h-full border-3 rounded-md border-[#E0E0E0]"
       />
-      <h1 class="pt-4">{title}</h1>
+      <h1 class="pt-4">
+        <b>{title}</b>
+      </h1>
       <span class="pt-2">{subTitle}</span>
     </div>
   );
 }
 
 export default function Banners({ banners, title }: Props) {
+  const id = `banner-${useId()}`;
   return (
     <section>
       <div class="container flex flex-col justify-center items-center gap-10 ">
         {/*add max width*/}
         <h1 class="">{title}</h1>
-        <div class="grid lg:grid-cols-4 gap-x-11 justify-center items-center">
+        <div class="lg:grid hidden lg:grid-cols-4 gap-x-11 justify-center items-center">
           {banners.map((item) => <Banner {...item} />)}
+        </div>
+        <div
+          id={id}
+          class="lg:hidden justify-center items-center grid pb-2 grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_64px] mb-20"
+        >
+          <Slider class="flex snap-mandatory pl-3 scroll-smooth scrollbar overflow-x-scroll w-full col-span-full row-span-full pb-8 gap-6">
+            {banners?.map((banner, index) => (
+              <Slider.Item
+                index={index}
+                class="carousel-item last:pr-3 w-[310px]"
+              >
+                <Banner {...banner} />
+              </Slider.Item>
+            ))}
+          </Slider>
+
+          <SliderJS rootId={id} />
         </div>
       </div>
     </section>
